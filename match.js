3345c31cc0aaf0ed77a3cf59dfc5b6d67ab3345f
AFNsclub <sisaif167@gmail.com>
2026-02-02 20:16:33 +0600

    Update match.js

--- match.js
+++ match.js
@@ -1,40 +1,14 @@
-let players = JSON.parse(localStorage.getItem("players")) || [];
+function matchResult(winner, loser){
+    let players = JSON.parse(localStorage.players);
 
-const a = document.getElementById("playerA");
-const b = document.getElementById("playerB");
-const w = document.getElementById("winner");
+    let w = players.find(p=>p.username===winner);
+    let l = players.find(p=>p.username===loser);
 
-function loadPlayers() {
-  players.forEach(p => {
-    a.innerHTML += `<option>${p.name}</option>`;
-    b.innerHTML += `<option>${p.name}</option>`;
-    w.innerHTML += `<option>${p.name}</option>`;
-  });
-}
+    w.rating += 20;
+    w.wins++;
 
-loadPlayers();
+    l.rating -= 15;
+    l.losses++;
 
-function submitMatch() {
-  let pa = a.value;
-  let pb = b.value;
-  let win = w.value;
-
-  if (pa === pb) {
-    alert("Same player not allowed");
-    return;
-  }
-
-  players = players.map(p => {
-    if (p.name === win) {
-      p.rating = Math.min(100, p.rating + 10);
-      p.win++;
-    } else if (p.name === pa || p.name === pb) {
-      p.rating = Math.max(0, p.rating - 5);
-      p.lose++;
-    }
-    return p;
-  });
-
-  localStorage.setItem("players", JSON.stringify(players));
-  document.getElementById("msg").innerText = "Match Saved Successfully!";
+    localStorage.players = JSON.stringify(players);
 }
