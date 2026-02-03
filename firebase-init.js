8dc9c7435b24d007eed36a910f0fae7c3b80ec1b
AFNsclub <sisaif167@gmail.com>
2026-02-03 21:05:14 +0600

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,16 +1,14 @@
-// firebase-init.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBtDxu0LJyb10ZkhH8IpxT5s8PcKc4nUxM",
+  apiKey: "AIzaSyBtXDuXLJyb10ZkHH8lpxT5",
   authDomain: "afnsclub.firebaseapp.com",
   projectId: "afnsclub",
-  storageBucket: "afnsclub.firebasestorage.app",
+  storageBucket: "afnsclub.appspot.com",
   messagingSenderId: "1088089213558",
-  appId: "1:1088089213558:web:bd5e01caaeecaa46bcad57",
-  measurementId: "G-WT4XY15N6Y"
+  appId: "1:1088089213558:web:bd5e01caaeeca46bcad57"
 };
 
 export const app = initializeApp(firebaseConfig);
